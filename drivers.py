45ac73c7951c4b8095b048f64d0791997994d7d7
HongyanYang825 <103646991+HongyanYang825@users.noreply.github.com>
2022-04-16 00:06:52 -0400

    Add files via upload

--- drivers.py
+++ drivers.py
@@ -7,6 +7,47 @@
 '''
 
 
+def check_input(eg_list, lens_list):
+    if len(eg_list) != len(lens_list):
+        raise ValueError("Please input all edges' lengths.")
+    else:
+        ckd_egs, ckd_lens = [], []
+        for i in range(len(eg_list)):
+            temp_list = [each.upper() for each in eg_list[i]]
+            sorted_eg = "".join(sorted(temp_list))
+            if sorted_eg not in ckd_egs:
+                ckd_egs.append(sorted_eg)
+                ckd_lens.append(lens_list[i])
+            elif ckd_lens[ckd_egs.index(sorted_eg)] == lens_list[i]:
+                continue
+            else:
+                raise ValueError(f"Conflict lens input for edge {sorted_eg}.")
+                print("2")
+    return ckd_egs, ckd_lens
+
+def check_cycle():
+    pass
+
+def generate_subsets(vertices_set):
+    sets_dict, order = {}, len(vertices_set)
+    vert_list = list(vertices_set)
+    if order < 3:
+        return None
+    for i in range(2 ** order):
+        to_bin = bin(i).replace("0b", "")
+        bin_adj = (order - len(to_bin)) * "0" + to_bin
+        subset = [vert_list[i] for i in range(order) if bin_adj[i] == "1"]
+        key = sum([int(each) for each in bin_adj])
+        if key in sets_dict:
+            sets_dict[key].append(subset)
+        else:
+            sets_dict[key] = [subset]
+    return sets_dict
+        
+        
+    
+
+    
 def to_vertices_set(edges_list):
     vertices = []
     for i in range(len(edges_list)):
