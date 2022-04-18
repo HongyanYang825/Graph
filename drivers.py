38d8bceb754b847da151598ef861ff351ea3e295
HongyanYang825 <103646991+HongyanYang825@users.noreply.github.com>
2022-04-18 18:36:28 -0400

    Edited docstrings

--- drivers.py
+++ drivers.py
@@ -1,7 +1,10 @@
 '''
     DS 5010
     Spring 2022
-    Project drivers
+    Project_Drivers_Applications_of_Graph_Theory_Topics
+
+    Define a bunch of drivers to support the Graph class in
+    graph.py
 
     Hongyan Yang
 '''
@@ -10,32 +13,57 @@
 from itertools import permutations
 
 def check_input(eg_list, lens_list):
+    '''
+    Function -- check_input
+    Check user inputs of edges and edge_lens for a Graph instance,
+    transform inputs to correct format and raise errors when needed
+    Parameters: eg_list (list)  -- a list of all edges in a graph
+                lens_list (list) -- a list of ordered edge lengths      
+    Return user inputs in correct format. Raise error and return None
+    when inputs contains ValueError
+    '''
     if len(eg_list) != len(lens_list):
+        # Raise error when not every input edge has been assigned
+        # one and only one edge length
         raise ValueError("Please input all edges' lengths.")
     else:
         ckd_egs, ckd_lens = [], []
         for i in range(len(eg_list)):
+            # Transform input edges to uppercases
             temp_list = [each.upper() for each in eg_list[i]]
+            # Transform input edges to sorted order
             sorted_eg = "".join(sorted(temp_list))
             if sorted_eg not in ckd_egs:
                 ckd_egs.append(sorted_eg)
                 ckd_lens.append(lens_list[i])
             elif ckd_lens[ckd_egs.index(sorted_eg)] == lens_list[i]:
+                # Remove duplicated input edge length of the same edge
                 continue
             else:
+                # Raise error when the same edge has been assigned
+                # different edge lengths
                 raise ValueError(f"Conflict lens input for edge {sorted_eg}.")
     return ckd_egs, ckd_lens
 
 def generate_subsets(vertices_set):
+    '''
+    Function -- generate_subsets
+    Grnerate all subsets, or the power set of a given set
+    Parameters: vertices_set (set) -- a set contains all vertices of a given
+                                      Graph instance        
+    Returns a dictionary with keys as the sizes of the subsets and with value
+    as a list of all subsets of the same size
+    '''
     sets_dict, order = {}, len(vertices_set)
-    vert_list = list(vertices_set)
-    if order < 3:
-        return None
+    vert_list = list(vertices_set)  # Convert set to list for ordered elements
+    # Traverse all subsets in the power set
     for i in range(2 ** order):
+        # Convert the index to a binary number and format it
         to_bin = bin(i).replace("0b", "")
         bin_adj = (order - len(to_bin)) * "0" + to_bin
+        # Find the corresponding subset to the binary index number
         subset = [vert_list[i] for i in range(order) if bin_adj[i] == "1"]
-        key = sum([int(each) for each in bin_adj])
+        key = sum([int(each) for each in bin_adj])  # Key as the size of subset
         if key in sets_dict:
             sets_dict[key].append(subset)
         else:
@@ -43,42 +71,85 @@ def generate_subsets(vertices_set):
     return sets_dict
 
 def sort_edge(cycle_set):
+    '''
+    Function -- sort_edge
+    Sort edges' names in the cycle_set to ascending order
+    Parameters: cycle_set (set) -- a set of edges in a cycle, edges' names are
+                                   unformatted
+    Return a formatted set of edges in a cycle
+    '''
     sorted_set, eg_list = set(), list(cycle_set)
+    # Format all edges' names in the cycle_set to ascending order
     for i in range(len(cycle_set)):
         temp_list = [each.upper() for each in eg_list[i]]
         sorted_eg = "".join(sorted(temp_list))
+        # Add the edge with its name sorted to output set
         sorted_set.add(sorted_eg)
     return sorted_set
 
 def generate_cycle(vertices_list):
+    '''
+    Function -- generate_cycle
+    Generate a set of edges with formatted names given the input vertices
+    Parameters: vertices_list (list)  -- A list of vertices from a subset of
+                                         vertices of the vertices_set
+    Return a set of edges in a cycle, edges are formatted
+    '''
     cycle_set = set()
+    # Generate a list of edges in arranged order
     for i in range(len(vertices_list)):
         if i != len(vertices_list) - 1:
             cycle_set.add(vertices_list[i] + vertices_list[i + 1])
         else:
             cycle_set.add(vertices_list[0] + vertices_list[i])
+    # Format edges' names in the output set of edges
     cycle_set = sort_edge(cycle_set)
     return cycle_set
 
 def reduce_duplicate_cycle(vertices_set):
+    '''
+    Function -- reduce_duplicate_cycle
+    Remove duplicated cycles by the nature of cycle given a set of vertices 
+    Parameters: vertices_set (set) -- a set contains all vertices of a given
+                                      subset of the graph's vertices_set     
+    Return a list of nonduplicated cycles given its vertices
+    '''
     unique_cycles = []
+    # Traverse all possible cycles given its vertices
     for each in list(permutations(vertices_set)):
+        # Generate a set of edges with formatted names
         cycle_set = generate_cycle(each)
         if cycle_set not in unique_cycles:
-            unique_cycles.append(cycle_set)
+            unique_cycles.append(cycle_set) # Append only unique set of edges
     return unique_cycles
 
 def check_cycle(eg_list, vertices_set):
+    '''
+    Function -- check_cycle
+    Check and return all existing cycles in the given graph
+    Parameters: eg_list (list)  -- a list of all edges in a graph
+                vertices_set (set) -- a set contains all vertices
+                                      of a given Graph instance                             
+    Returns a dictionary with keys as the sizes of the cycles and
+    values as a list of all existing cycles with given size
+    '''
     eg_set, cycle_dict = set(eg_list), {}
+    # Generate a dictionary with keys as the sizes of the subsets and
+    # values as a list of all subsets of the same size
     vertices_subsets = generate_subsets(vertices_set)
-    if len(vertices_subsets) < 3:
-        return None
+    # Return an empty dictionary if there's no potential cycles 
+    if len(vertices_subsets) < 4:
+        return {}
+    # Only check subsets with more than 3 vertices inside
     potential_subsets = {key: vertices_subsets[key]
                          for key in range(3, len(vertices_subsets))}
+    # Traverse all potential cycles in the given graph
     for key in potential_subsets.keys():
         for value in potential_subsets[key]:
+            # Remove duplicated cycles by the nature of cycle
             unique_cycles = reduce_duplicate_cycle(value)
             for each in unique_cycles:
+                # Check if the unique set of edges exists in the graph
                 if each.issubset(eg_set):
                     if key in cycle_dict:
                         cycle_dict[key].append(each)
@@ -87,6 +158,16 @@ def check_cycle(eg_list, vertices_set):
     return cycle_dict
 
 def generate_path(path_tuple, sequence_tuple):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     path_of_egs, sequence_list = [], list(sequence_tuple)
     path_tuple = tuple(sorted(list(path_tuple)))
     sequence_list.insert(0, path_tuple[0])
@@ -97,6 +178,16 @@ def generate_path(path_tuple, sequence_tuple):
     return path_of_egs, set(path_of_egs)
 
 def print_path(path_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     path_route = ""
     for i in range(len(path_list) - 1):
         path_route += f"{path_list[i]} --> "
@@ -104,18 +195,48 @@ def print_path(path_list):
     return path_route
 
 def path_len(path_list, eg_list, lens_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     total_len = 0
     for each in path_list:
         total_len += lens_list[eg_list.index(each)]
     return total_len
 
 def min_index(path_len_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     min_len = min(path_len_list)
     min_indices = [index for index in range(len(path_len_list))
                    if path_len_list[index] == min_len]
     return min_indices, min_len
 
 def shortest_path(path_tuple, eg_list, lens_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     potential_paths, path_lens, shortest_paths = [], [], []
     path_tuple = tuple([each.upper() for each in path_tuple])
     vertices_set = to_vertices_set(eg_list)
@@ -138,6 +259,16 @@ def shortest_path(path_tuple, eg_list, lens_list):
         return shortest_paths, min_len
 
 def to_vertices_set(edges_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     vertices = []
     for i in range(len(edges_list)):
         vertices.extend(list(edges_list[i]))
@@ -145,6 +276,16 @@ def to_vertices_set(edges_list):
     return vertices_set
 
 def connect_indices(edge, edges_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     indices = []
     for i in range(len(edges_list)):
         if not set(edge).isdisjoint(set(edges_list[i])):
@@ -152,6 +293,16 @@ def connect_indices(edge, edges_list):
     return indices
 
 def append_by_index(indices_list, from_list, to_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     for index in sorted(indices_list, reverse = True):
         to_append = from_list.pop(index)
         if to_append not in to_list:
@@ -159,6 +310,16 @@ def append_by_index(indices_list, from_list, to_list):
     return to_list
  
 def append_by_values(value_list, from_list, to_list):
+    '''
+    Function -- load_window
+    Draw a window at preset position with adjustable side_length
+    Parameters: window (str)  -- current window to load
+                start_point (tuple) -- start position
+                horizontal (float) -- window's horizontal length
+                vertical (float) -- window's vertical length
+                width, color, speed -- default pen information        
+    Draw a window at pre-determined position with specific size
+    '''
     output_list = []
     for value in value_list:
         index = from_list.index(value)
---
+++ graph.py
@@ -0,0 +1,157 @@
+'''
+    DS 5010
+    Spring 2022
+    Project_Applications_of_Graph_Theory_Topics
+
+    Define and implement a Graph class to conduct graph operations
+    based on the Graph Theory
+
+    Hongyan Yang
+'''
+
+
+from drivers import *
+
+class Graph:
+    def __init__(self, edges, edge_lens, is_tree = False):
+        '''
+        Method -- __init__
+        Create a new Graph instance by supplying edges and edge_lens
+        for all edges in a graph with edge lenths set accordingly
+        Parameters: edges (list) -- a list of all edges in a graph
+                    edge_lens (list) -- a list of ordered edge lengths
+                    is_tree (bool) -- a boolean indicates if it is a tree
+        Create a new Graph instance and set a bunch of default attributes
+        '''
+        ckd_egs, ckd_lens = check_input(edges, edge_lens)
+        self.edges = ckd_egs
+        self.edge_lens = ckd_lens
+        self.is_tree = is_tree
+        # Set vertices attibute with get_vertices() method
+        self.vertices = self.set_vertices()
+        self.size = len(self.edges)
+        self.order = len(self.vertices)
+        # List all connected subgraphs as a dictionary
+        # given the created Graph instance
+        self.connected_subgraphs = self.set_subgraphs()
+
+    def set_edges_and_lens(self, edges, edge_lens):
+        '''
+        Method -- set_edges_and_lens
+        A method to check and set graph's edges and their corresponding
+        edge lengths
+        Parameters: self -- input Graph instance
+                    edges (list) -- a list of all edges in a graph
+                    edge_lens (list) -- a list of ordered edge lengths
+        Check and set edges and edge_lens attribute for a Graph instance
+        '''
+        ckd_egs, ckd_lens = check_input(edges, edge_lens)
+        self.edges = ckd_egs
+        self.edge_lens = ckd_lens
+
+    def set_vertices(self):
+        '''
+        Method -- set_vertices
+        Extract vertices info given a graph's all edges
+        Parameters: self -- input Graph instance
+        Return a set of graph vertices
+        '''
+        vertices = []
+        # Add all nodes of existing edges to the list
+        for i in range(len(self.edges)):
+            vertices.extend(list(self.edges[i]))
+        # Return a set of vertices to remove duplicates
+        return set(vertices)
+
+    def set_subgraphs(self):
+        '''
+        Method -- set_subgraphs
+        Find all connected subgraphs in the given Graph instance and find
+        their edge lenghs and vertices accordingly
+        Parameters: self -- input Graph instance
+        Return a dictionary with keys as the index of a certain sub_graph
+        and values as its edges, edges' corresponding lengths and vertices
+        '''
+        dict, i = {}, 0
+        eg_list, len_list = self.edges[:], self.edge_lens[:]
+        # Keep searching new subgraphs while there're edges remaining
+        while len(eg_list) != 0:
+            # Create a new item in dict for each subgraph
+            dict[i] = ([eg_list[0]], [], set(eg_list[0]))
+            # Get the indices of edges connecting to the subgraph
+            indices = connect_indices(dict[i][2], eg_list)
+            # Keep adding edges to the subgraph until all edges are compared
+            while len(indices) != 0:
+                # Append conncted edges to the subgraph
+                append_by_index(indices, eg_list, dict[i][0])
+                # Update the subgraph's vertices set
+                dict[i][2].update(to_vertices_set(dict[i][0]))
+                indices = connect_indices(dict[i][2], eg_list)
+            # Form a list of corresponding edge lengths to the subgraph
+            # Extend the list to subgraphs dictionary
+            sub_lens = append_by_values(dict[i][0], self.edges, self.edge_lens)
+            dict[i][1].extend(sub_lens)
+            i += 1
+        return dict
+
+    def get_edges(self):
+        '''
+        Method -- get_edges
+        A get method to get graph's edges
+        Parameters: self -- input Graph instance
+        Return edges attribute of a Graph instance
+        '''
+        return self.edges
+
+    def get_edge_lens(self):
+        '''
+        Method -- get_edge_lens
+        A get method to get graph's edge_lens
+        Parameters: self -- input Graph instance
+        Return edge_lens attribute of a Graph instance
+        '''
+        return self.edge_lens
+
+    def get_vertices(self):
+        '''
+        Method -- get_vertices
+        A get method to get graph's vertices
+        Parameters: self -- input Graph instance
+        Return vertices attribute of a Graph instance
+        '''
+        self.vertices = self.set_vertices()
+        return self.vertices
+
+    def get_size(self):
+        '''
+        Method -- get_size
+        A get method to get graph's size
+        Parameters: self -- input Graph instance
+        Return size attribute of a Graph instance
+        '''
+        self.size = len(self.edges)
+        return self.size
+
+    def get_order(self):
+        '''
+        Method -- get_order
+        A get method to get graph's order
+        Parameters: self -- input Graph instance
+        Return order attribute of a Graph instance
+        '''
+        self.vertices = self.set_vertices()
+        self.order = len(self.vertices)
+        return self.order
+
+    def get_connected_subgraphs(self):
+        '''
+        Method -- get_connected_subgraphs
+        A get method to get graph's all connected_subgraphs
+        Parameters: self -- input Graph instance
+        Return connected_subgraphs attribute of a Graph instance
+        '''
+        self.connected_subgraphs = self.set_subgraphs()
+        return self.connected_subgraphs
+
+    def cycles(self):
+        return check_cycle(self.edges, self.vertices)
