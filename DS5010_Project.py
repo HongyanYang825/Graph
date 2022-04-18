7732d34cc4d8c253bf6f8ddcab4706bab183feb3
HongyanYang825 <103646991+HongyanYang825@users.noreply.github.com>
2022-04-18 13:51:25 -0400

    Add docstrings and all get, set methods

--- DS5010_Project.py
+++ DS5010_Project.py
@@ -27,38 +27,50 @@ class Graph:
         self.edge_lens = edge_lens
         self.is_tree = is_tree
         # Set vertices attibute with get_vertices() method
-        self.vertices = self.get_vertices()
-        self.size = len(edges)
+        self.vertices = self.set_vertices()
+        self.size = len(self.edges)
         self.order = len(self.vertices)
         # List all connected subgraphs as a dictionary
         # given the created Graph instance
-        self.connected_subgraphs = self.connected_subgraphs()
+        self.connected_subgraphs = self.set_subgraphs()
 
-    def set_vertices(self, vertices):
+    def set_edges(self, edges):
+        '''
+        Method -- set_edges
+        A set method to set graph's edges
+        Parameters: self -- input Graph instance
+                    edges (list) -- a list of all edges in a graph
+        Set edges attribute for a Graph instance
+        '''
+        self.edges = edges
+
+    def set_edge_lens(self, edge_lens):
+        '''
+        Method -- set_edge_lens
+        A set method to set graph's edge_lens
+        Parameters: self -- input Graph instance
+                    edges (list) -- a list of ordered edge lengths
+        Set edge_lens attribute for a Graph instance
+        '''
+        self.edge_lens = edge_lens
+
+    def set_vertices(self):
         '''
         Method -- set_vertices
-        A set method to set graph's vertices manually
-        Parameters: self -- input Graph instance
-                    vertices (set) -- a set of graph vertices
-        Set vertices value for a Graph instance
-        '''
-        self.vertices = vertices
-
-    def get_vertices(self):
-        '''
-        Method -- get_vertices
         Extract vertices info given a graph's all edges
         Parameters: self -- input Graph instance
         Return a set of graph vertices
         '''
         vertices = []
+        # Add all nodes of existing edges to the list
         for i in range(len(self.edges)):
             vertices.extend(list(self.edges[i]))
+        # Return a set of vertices to remove duplicates
         return set(vertices)
 
-    def connected_subgraphs(self):
+    def set_subgraphs(self):
         '''
-        Method -- connected_graphs
+        Method -- set_subgraphs
         Find all connected subgraphs in the given Graph instance and find
         their edge lenghs and vertices accordingly
         Parameters: self -- input Graph instance
@@ -67,14 +79,81 @@ class Graph:
         '''
         dict, i = {}, 0
         eg_list, len_list = self.edges[:], self.edge_lens[:]
+        # Keep searching new subgraphs while there're edges remaining
         while len(eg_list) != 0:
+            # Create a new item in dict for each subgraph
             dict[i] = ([eg_list[0]], [], set(eg_list[0]))
+            # Get the indices of edges connecting to the subgraph
             indices = connect_indices(dict[i][2], eg_list)
+            # Keep adding edges to the subgraph until all edges are compared
             while len(indices) != 0:
+                # Append conncted edges to the subgraph
                 append_by_index(indices, eg_list, dict[i][0])
+                # Update the subgraph's vertices set
                 dict[i][2].update(to_vertices_set(dict[i][0]))
                 indices = connect_indices(dict[i][2], eg_list)
+            # Form a list of corresponding edge lengths to the subgraph
+            # Extend the list to subgraphs dictionary
             sub_lens = append_by_values(dict[i][0], self.edges, self.edge_lens)
             dict[i][1].extend(sub_lens)
             i += 1
         return dict
+
+    def get_edges(self):
+        '''
+        Method -- get_edges
+        A get method to get graph's edges
+        Parameters: self -- input Graph instance
+        Return edges attribute of a Graph instance
+        '''
+        return self.edges
+
+    def get_edge_lens(self):
+        '''
+        Method -- get_edge_lens
+        A get method to get graph's edge_lens
+        Parameters: self -- input Graph instance
+        Return edge_lens attribute of a Graph instance
+        '''
+        return self.edge_lens
+
+    def get_vertices(self):
+        '''
+        Method -- get_vertices
+        A get method to get graph's vertices
+        Parameters: self -- input Graph instance
+        Return vertices attribute of a Graph instance
+        '''
+        self.vertices = self.set_vertices()
+        return self.vertices
+
+    def get_size(self):
+        '''
+        Method -- get_size
+        A get method to get graph's size
+        Parameters: self -- input Graph instance
+        Return size attribute of a Graph instance
+        '''
+        self.size = len(self.edges)
+        return self.size
+
+    def get_order(self):
+        '''
+        Method -- get_order
+        A get method to get graph's order
+        Parameters: self -- input Graph instance
+        Return order attribute of a Graph instance
+        '''
+        self.vertices = self.set_vertices()
+        self.order = len(self.vertices)
+        return self.order
+
+    def get_connected_subgraphs(self):
+        '''
+        Method -- get_connected_subgraphs
+        A get method to get graph's all connected_subgraphs
+        Parameters: self -- input Graph instance
+        Return connected_subgraphs attribute of a Graph instance
+        '''
+        self.connected_subgraphs = self.set_subgraphs()
+        return self.connected_subgraphs
