40ca4f5265dccacbef58817cf1f4d02740f582fd
HongyanYang825 <103646991+HongyanYang825@users.noreply.github.com>
2022-04-28 21:59:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author = "Hongyan Yang",
     author_email = "yang.hongy@northeastern.edu",
     description = "Graph offers solutions to applied graph theory problems and visualizes tree graphs in an interactive way.",
-    url = "https://github.com/HongyanYang825/DS5010_Project/tree/main/Graph",
+    url = "https://github.com/HongyanYang825/DS5010_Project",
     license = 'MIT',
     python_requires = '>=3',
 )
